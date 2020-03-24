8570f138d63acbd59da53564b35813573d54db43
Suresh Khatri <servlet2@msn.com>
2020-03-25 00:51:03 +1100

    update codestyle configuration

--- gradle/includes/codestyle.gradle.kts
+++ gradle/includes/codestyle.gradle.kts
@@ -9,6 +9,10 @@ buildscript {
 apply(plugin = "checkstyle")
 
 tasks.withType<Checkstyle>().configureEach {
+    ignoreFailures = false
+    maxErrors = 0
+    maxWarnings = 0
+
     configFile = file("$rootDir/gradle/settings/checkstyle.xml")
     reports {
         xml.isEnabled = false
