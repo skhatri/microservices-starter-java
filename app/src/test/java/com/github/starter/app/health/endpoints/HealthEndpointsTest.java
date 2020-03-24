43256a8a08f46b0aa6b1b90cfb8859d73dc2b35f
Suresh Khatri <servlet2@msn.com>
2020-03-25 00:57:04 +1100

    update health endpoint test to use mono consumer

---
+++ app/src/test/java/com/github/starter/app/health/endpoints/HealthEndpointsTest.java
@@ -0,0 +1,64 @@
+package com.github.starter.app.health.endpoints;
+
+import com.github.starter.core.consumer.MonoConsumer;
+import java.util.Map;
+import java.util.function.Predicate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+
+@DisplayName("Health Endpoints Test")
+@ExtendWith(SpringExtension.class)
+public class HealthEndpointsTest {
+
+    private WebTestClient webTestClient;
+
+    @Test
+    @DisplayName("test liveness endpoint")
+    public void testLivenessEndpoint() {
+        String uri = "/liveness";
+
+        WebTestClient webTestClient = WebTestClient.bindToController(HealthEndpoints.class)
+                .build();
+        verifyResult(uri, webTestClient, Map.class, m -> !m.isEmpty());
+    }
+
+    private <T> void verifyResult(String uri, WebTestClient webTestClient, Class<T> clz, Predicate<T> predicate) {
+        Mono<T> result = Mono.from(webTestClient.get().uri(uri).exchange().expectStatus().isOk().returnResult(clz).getResponseBody());
+        new MonoConsumer<>(result, false).drain(res -> Assertions.assertTrue(predicate.test(res)));
+        StepVerifier.create(result)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("test readiness endpoint")
+    public void testReadinessEndpoint() {
+        String uri = "/readiness";
+        WebTestClient webTestClient = WebTestClient.bindToController(HealthEndpoints.class).build();
+        verifyResult(uri, webTestClient, Map.class, m -> !m.isEmpty());
+    }
+
+    @Test
+    @DisplayName("test index endpoint")
+    public void testIndexEndpoint() {
+        String uri = "/";
+        WebTestClient webTestClient = WebTestClient.bindToController(HealthEndpoints.class).build();
+        verifyResult(uri, webTestClient, Map.class, m -> !m.isEmpty());
+    }
+
+
+    @Test
+    @DisplayName("test non-existent endpoint")
+    public void testNonExistent() {
+        String uri = "/readiness-xyz";
+        WebTestClient webTestClient = WebTestClient.bindToController(HealthEndpoints.class)
+                .build();
+        webTestClient.get().uri(uri).exchange().expectStatus().isNotFound();
+    }
+}
