d65fcd958a9b044ca4917a4d6130e4fb4e21b6ec
Suresh Khatri <servlet2@msn.com>
2020-03-25 00:57:04 +1100

    update health endpoint test to use mono consumer

---
+++ app/src/test/java/com/github/starter/core/consumer/MonoConsumer.java
@@ -0,0 +1,43 @@
+package com.github.starter.core.consumer;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.function.Consumer;
+import reactor.core.publisher.Mono;
+
+public class MonoConsumer<T> {
+    private Mono<T> mono;
+    private boolean error;
+
+    public MonoConsumer(Mono<T> mono, boolean error) {
+        this.mono = mono;
+        this.error = error;
+    }
+
+    public void drain(Consumer<T> consumer) {
+        CountDownLatch latch = new CountDownLatch(1);
+        mono.subscribe(res -> {
+                if (consumer != null) {
+                    consumer.accept(res);
+                }
+                if (!error) {
+                    latch.countDown();
+                }
+            },
+            err -> {
+                if (error) {
+                    latch.countDown();
+                }
+            }
+        );
+
+        try {
+            latch.await();
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void drain() {
+        drain(null);
+    }
+}
