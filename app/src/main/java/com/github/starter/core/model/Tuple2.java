f50ca28e035c8260437bb0d7561dc0f058364b08
Suresh Khatri <servlet2@msn.com>
2020-03-26 03:29:10 +1100

    pair implementation

---
+++ app/src/main/java/com/github/starter/core/model/Tuple2.java
@@ -0,0 +1,12 @@
+package com.github.starter.core.model;
+
+public class Tuple2<A, B> {
+    public final A first;
+    public final B second;
+
+    public Tuple2(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+}
