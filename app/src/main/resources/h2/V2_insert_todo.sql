bc6f09c64c9b61575ac02644f5898f9c977a5b98
Suresh Khatri <servlet2@msn.com>
2020-03-27 02:55:06 +1100

    load test

--- app/src/main/resources/h2/V2_insert_todo.sql
+++ app/src/main/resources/h2/V2_insert_todo.sql
@@ -1,5 +1,31 @@
 insert into todo.tasks(id, description, action_by, created, status)
-values('1', 'Task 1', 'user1', '2020-03-20 00:00:00', 'NEW');
+values('1', 'Buy Groceries', 'user1', '2020-03-20 00:00:00', 'NEW');
 
 insert into todo.tasks(id, description, action_by, created, status, updated)
-values('2', 'Task 2', 'user1', '2020-03-20 13:00:00', 'NEW', '2020-03-20 13:00:00');
+values('2', 'Watch Movie', 'user1', '2020-03-20 13:00:00', 'NEW', '2020-03-20 13:00:00');
+
+
+insert into todo.tasks(id, description, action_by, created, status, updated)
+values('3', 'Read', 'user1', '2020-03-20 13:00:00', 'NEW', '2020-03-20 13:00:00');
+
+
+insert into todo.tasks(id, description, action_by, created, status, updated)
+values('4', 'Dance', 'user1', '2020-03-20 13:00:00', 'NEW', '2020-03-20 13:00:00');
+
+insert into todo.tasks(id, description, action_by, created, status, updated)
+values('5', 'Go for Walk', 'user1', '2020-03-20 13:00:00', 'DONE', '2020-03-20 13:00:00');
+
+insert into todo.tasks(id, description, action_by, created, status, updated)
+values('6', 'Gym', 'user1', '2020-03-20 13:00:00', 'NEW', '2020-03-20 13:00:00');
+
+insert into todo.tasks(id, description, action_by, created, status, updated)
+values('7', 'Do Yoga', 'user1', '2020-03-20 13:00:00', 'NEW', '2020-03-20 13:00:00');
+
+insert into todo.tasks(id, description, action_by, created, status, updated)
+values('8', 'Chat with Friends', 'user1', '2020-03-20 13:00:00', 'NEW', '2020-03-20 13:00:00');
+
+insert into todo.tasks(id, description, action_by, created, status, updated)
+values('9', 'Listen to Spotify', 'user1', '2020-03-20 13:00:00', 'NEW', '2020-03-20 13:00:00');
+
+insert into todo.tasks(id, description, action_by, created, status, updated)
+values('10', 'Sleep until Late', 'user1', '2020-03-20 13:00:00', 'NEW', '2020-03-20 13:00:00');
