19577e6673520f1568f0731f155501b867c4223c
Suresh Khatri <servlet2@msn.com>
2020-04-10 23:03:31 +1000

    add cors header for todo endpoint

--- app/src/main/java/com/github/starter/app/todo/endpoints/TodoEndpoints.java
+++ app/src/main/java/com/github/starter/app/todo/endpoints/TodoEndpoints.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/todo")
+@CrossOrigin(value = "*")
 public class TodoEndpoints {
 
     private final TodoService todoService;
